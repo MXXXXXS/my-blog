1476d6955107b7af63d7df291d629f1d7dfe3a8c
mxxxxxs <relife_abyss@qq.com>
2018-08-26 18:38:21 +0800

    命令回溯功能√

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,11 @@
-const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const md = require('markdown-it')()
+const express = require('express')
 const app = express()
+const articleDir = 'D:/coding/AQUAPage/articles/'
+const frontEndDir = 'D:/coding/AQUAPage/frontEnd'
+const imagesDir = 'D:/coding/AQUAPage/images'
 
 // app.get('/', (rq, rs) => {
 //   rs.send('hello world!')
@@ -9,11 +14,33 @@ const app = express()
 app.get('/article/*', (rq, rs) => {
   let article = path.basename(rq.path)
   console.log(article)
-  rs.send('received article name: ' + article)
+  fs.readFile(articleDir + article, (err, data) => {
+    // console.log('../article/' + article)
+    if (err) {
+      rs.send(`emmmm, 可能没有一篇叫"${article}"的文章. 参考错误: ${err}`)
+      return
+    }
+    console.log(data.toString())
+
+    let result = md.render(data.toString())
+    rs.send(result)
+    // rs.send('服务器已接受: ' + article)
+  })
+  // rs.send('received article name: ' + article)
 })
 
-app.use(express.static('../frondEnd'))
-// app.use(express.static('../images'))
+app.get('/articleList', (rq, rs) => {
+  fs.readdir(articleDir, (err, files) => {
+    if (err) {
+      rs.send(`!!!, 读取文章列表居然失败了? 参考错误: ${err}`)
+      return
+    }
+    rs.send(JSON.stringify(files))
+  })
+})
+
+app.use(express.static(frontEndDir))
+app.use('/images', express.static(imagesDir))
 
 
 const server = app.listen(80, () => {
